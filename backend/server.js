7bbe436ccdf2928e58cc42fa69d461c1d7803d7e
Prakash Mishra <pm9507041@gmail.com>
2021-10-15 19:24:44 +0530

    Created Connections for database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const app = require('./app');
 
 const dotenv = require('dotenv');
+const connectDataBase = require('./config/database')
 
 //config
 dotenv.config({path:"backend/config/config.env"});
+
+//connecting to database
+connectDataBase()
 
 app.listen(process.env.PORT, () => {
     console.log(`server working on http://localhost:${process.env.PORT}`)
