4e9e16fb70751aec9245bed22da78068d739c52a
Prakash Mishra <pm9507041@gmail.com>
2021-10-18 08:26:35 +0530

    Filtering and Pagination

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,6 +1,7 @@
 const Product = require('../models/productModel');
 const ErrorHandler = require('../utils/errorhandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+const ApiFeatures = require('../utils/apifeatures');
 
 
 //Create Product --Admin
@@ -14,7 +15,10 @@ exports.createProduct = catchAsyncErrors( async (req, res, next) => {
 
 //get all product
 exports.getAllProduct = catchAsyncErrors(async (req, res) => {
-    const products = await Product.find();
+    const resultPerPage = 5
+    const productCount = await Product.countDocuments();
+    const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
+    const products = await apiFeature.query;
     res.status(200).json({
         success:true,
         products
@@ -31,7 +35,8 @@ exports.getProductDetails =catchAsyncErrors(async (req, res, next) => {
       
     res.status(200).json({
         success:true,
-        product
+        product,
+        productCount
     });
 });
 
