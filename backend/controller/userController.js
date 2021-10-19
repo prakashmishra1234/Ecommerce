0999a85565c0933ae5b1c0722a6b473aeb4145e4
Prakash Mishra <pm9507041@gmail.com>
2021-10-19 22:26:48 +0530

    Added login function and JWT and Cokkie

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,7 @@
 const ErrorHandler = require('../utils/errorhandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const User = require('../models/userModel');
+const sendToken = require('../utils/jwttoken');
 
 //Register a user
 exports.registerUser = catchAsyncErrors( async (req, res, next) => {
@@ -12,9 +13,23 @@ exports.registerUser = catchAsyncErrors( async (req, res, next) => {
             url: 'profilepicurl'
         }
     });
-    const token = user.getJWTToken();
-    res.status(201).json({
-        success: true,
-        token
-    })
-})
+    sendToken(user, 201, res);
+});
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const { email, password } = req.body;
+    // checking if user has given password and email both
+    if (!email || !password) {
+      return next(new ErrorHandler("Please Enter Email & Password", 400));
+    }
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return next(new ErrorHandler("Invalid email or password", 401));
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+    if (!isPasswordMatched) {
+      return next(new ErrorHandler("Invalid email or password", 401));
+    }
+    sendToken(user, 200, res);
+  });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -55,4 +55,9 @@ userSchema.methods.getJWTToken = function () {
     });
 };
 
+// Compare Password
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
+
 module.exports = mongoose.model('User', userSchema);
