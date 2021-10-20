490b6c6c5aa34f1f369f81d56eba6f648419b4a6
Prakash Mishra <pm9507041@gmail.com>
2021-10-20 18:59:43 +0530

    Created userAuthentication and authorizeRole function, logout functionality and added user in the productSchema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,70 +1,80 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema ({
-    name:{
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLength: [8, "Price cannot exceed 8 characters"],
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
         type: String,
-        required:[true, 'Please provide product name'],
-        trim:true
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
     },
-    description:{
-        type:String,
-        required:[true, 'Please provide product descriptions']
+  ],
+  category: {
+    type: String,
+    required: [true, "Please enter product category"],
+  },
+  Stock: {
+    type: Number,
+    required: [true, "Please enter product stock"],
+    maxLength: [4, "Stock cannot exceed 4 characters"],
+    default: 1,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
     },
-    price:{
-        type:Number,
-        required:[true, 'Please provide product price'],
-        maxlength:[8, 'Price cannot exceed 8 character']
-    },
-    ratings:{
-        type:Number,
-        default:0
-    },
-    images:[
-        {
-            public_id: {
-                type:String,
-                required:true
-            },
-            url: {
-                type:String,
-                required:true
-            }
-        }
-    ],
-    category:{
-        type:String,
-        required:[true, 'Please provide product category']
-    },
-    stock:{
-        type:Number,
-        required:[true, 'Please mention your stock'],
-        maxlength:[4, 'Stock cannot exceed 4 characters'],
-        default:1
-    },
-    numOfReviews:{
-        type:Number,
-        default:0
-    },
-    reviews:[
-        {
-            name:{
-                type:String,
-                required:true
-            },
-            rating:{
-                type:Number,
-                required:true
-            },
-            comment:{
-                typeL:String
-            }
-        }
-    ],
-    createdAt:{
-        type:Date,
-        default:Date.now
-    }
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-})
-
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema);
