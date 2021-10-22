f645a2809b4724fc5886ad99e6b33d7539b48e3d
Prakash Mishra <pm9507041@gmail.com>
2021-10-22 20:51:40 +0530

    Created resetPassword function and errorhandler for mongoose dulicate key error, jwt token error and jwt expire error

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -10,8 +10,26 @@ module.exports = (err, req, res, next) => {
         err = new ErrorHandler(message, 400);
 }
 
+//Mongoose duplicate key error
+if (err.code === 11000){
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`
+    err = new ErrorHandler(message, 400);
+}
+
+//jwt token error
+if(err.name === 'JsonWebTokenError') {
+    const message = `Json web token is invalid, try again`;
+    err = new ErrorHandler(message, 400);
+}
+
+//jwt expire error
+if(err.name === 'TokenExpiredError') {
+    const message = `Json web token is expired, try again`;
+    err = new ErrorHandler(message, 400);
+}
+
     res.status(err.statusCode).json({
         success:false,
-        message:err.stack
+        message:err.message
     })
 }
