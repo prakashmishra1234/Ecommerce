ef4514dbf431710c105096d601f48b3632a122ef
Prakash Mishra <pm9507041@gmail.com>
2021-10-23 16:42:57 +0530

    Created functions for Change password and change user details

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -118,3 +118,35 @@ exports.getUserDetails = catchAsyncErrors( async (req, res, next) => {
     user
   })
 });
+
+//Update Password
+exports.updatePassword = catchAsyncErrors( async (req, res, next) => {
+  const user = await User.findById(req.user.id).select('+password');
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+    if (!isPasswordMatched) {
+      return next(new ErrorHandler("Old password is incorrect", 400));
+    }
+    if (req.body.newPassword !== req.body.confirmPassword) {
+      return next(new ErrorHandler("Password does not match", 400))
+    }
+    user.password = req.body.newPassword
+    await user.save();
+    sendToken(user, 200, res);
+});
+
+//Update user profile
+exports.updateProfile = catchAsyncErrors( async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email
+  }
+//I will add cloudinary later
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false
+  });
+  res.status(200).json({
+    success: true
+  });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails } = require('../controller/userController');
+const { registerUser, loginUser, 
+    logout, forgotPassword, resetPassword, 
+    getUserDetails, updatePassword, updateProfile } = require('../controller/userController');
 const router = express.Router();
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
@@ -9,5 +11,7 @@ router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
 router.route('/logout').get(logout);
 router.route('/me').get(isAuthenticatedUser, getUserDetails);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 
 module.exports = router;
