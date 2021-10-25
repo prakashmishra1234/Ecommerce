adbe848cda2fea2104f850a0f6ca96bb4a574313
Prakash Mishra <pm9507041@gmail.com>
2021-10-25 22:26:09 +0530

    Created Home Component along with css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import WebFont from 'webfontloader';
 import React from 'react';
 import Footer from './component/layout/Footer/Footer.js';
+import Home from './component/Home/Home.js'
 
 function App() {
   React.useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
   return (
     <Router>
       <Header />
+        <Route exact path='/' component={Home} />
       <Footer />
     </Router>
   );
