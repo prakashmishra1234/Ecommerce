fb1e4b7cd72d54591a6c9d05e19b986cf5b447bd
Prakash Mishra <pm9507041@gmail.com>
2021-10-26 17:26:57 +0530

    Created loading component with css and errorhandler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import WebFont from 'webfontloader';
 import React from 'react';
 import Footer from './component/layout/Footer/Footer.js';
-import Home from './component/Home/Home.js'
+import Home from './component/Home/Home.js';
 
 function App() {
   React.useEffect(() => {
