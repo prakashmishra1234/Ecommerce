ac2442f39bfa9afa0b1bc50adcd986c9f7f5ca2a
Prakash Mishra <pm9507041@gmail.com>
2021-10-26 14:33:12 +0530

    Implimented redux created action, constant and reducers for products and fetched data from database

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -15,13 +15,14 @@ exports.createProduct = catchAsyncErrors( async (req, res, next) => {
 
 //get all product
 exports.getAllProduct = catchAsyncErrors(async (req, res) => {
-    const resultPerPage = 5
-    const productCount = await Product.countDocuments();
+    const resultPerPage = 8
+    const productsCount = await Product.countDocuments();
     const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination(resultPerPage);
     const products = await apiFeature.query;
     res.status(200).json({
         success:true,
-        products
+        products,
+        productsCount
     });
 });
 
--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,18 +1,20 @@
-import react, { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { CgMouse } from "react-icons/all";
 import './Home.css';
-import Product from './Product.js'
-
-const product = {
-    name: 'Blue Tshirt',
-    images: [{url: "https://i.ibb.co/DRST11n/1.webp"}],
-    price: "Rs 3000",
-    _id: "prakash"
-}
+import Product from './Product.js';
+import MetaData from '../layout/MetaData';
+import { getProduct } from '../../actions/productAction';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Home = () => {
+    const dispatch = useDispatch();
+    const {loading, error, products, productsCount} =useSelector((state) => state.products);
+    useEffect(() => {
+        dispatch(getProduct())
+    }, [dispatch]);
     return (
         <Fragment>
+            <MetaData title="ECOMMERCE"/>
             <div className='banner'>
                 <p>Welcome to Ecommerce</p>
                 <h1>FIND AMAZING PRODUCTS BELOW</h1>
@@ -24,15 +26,7 @@ const Home = () => {
             </div>
             <h2 className='homeHeading'>Featured Products</h2>
             <div className='container' id='container'>
-                <Product product={product} />
-                <Product product={product} />
-                <Product product={product} />
-                <Product product={product} />
-
-                <Product product={product} />
-                <Product product={product} />
-                <Product product={product} />
-                <Product product={product} />
+                {products && products.map((product) => <Product product={product} />)}
             </div>
         </Fragment>
     )
