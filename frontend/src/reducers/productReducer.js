7f337130a149302050912b30f04152e49d203202
Prakash Mishra <pm9507041@gmail.com>
2021-10-26 21:16:52 +0530

    Updated productAction, productConstant, productReducer and store for fetching product details

--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -1,5 +1,6 @@
 import {ALL_PRODUCT_FAIL, ALL_PRODUCT_SUCCESS, ALL_PRODUCT_REQUEST,
-    CLEAR_ERRORS} from '../constants/productConstants'
+    PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_SUCCESS,
+    CLEAR_ERRORS} from '../constants/productConstants';
 
 export const productReducer = (state={products:[]}, action) => {
     switch (action.type) {
@@ -15,6 +16,33 @@ export const productReducer = (state={products:[]}, action) => {
                 productsCount: action.payload.productsCount,
             };
         case ALL_PRODUCT_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            };
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null,
+            };
+        default:
+            return state;
+    }
+}
+
+export const productDetailsReducer = (state={product: {} }, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return {
+                loading: true,
+                ...state,
+            };
+        case PRODUCT_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                product: action.payload
+            };
+        case PRODUCT_DETAILS_FAIL:
             return {
                 loading: false,
                 error: action.payload,
