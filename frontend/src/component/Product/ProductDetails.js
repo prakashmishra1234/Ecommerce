5aa1b0c303c51cd894d8ddf50132c8df3722ef9c
Prakash Mishra <pm9507041@gmail.com>
2021-10-27 20:23:22 +0530

    Created ReviewCard and modified ProductDetails.js to display reviews and ratings

--- frontend/src/component/Product/ProductDetails.js
+++ frontend/src/component/Product/ProductDetails.js
@@ -4,6 +4,8 @@ import './ProductDetails.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { getProductDetails } from '../../actions/productAction';
 import ReactStars from 'react-rating-stars-component';
+import ReviewCard from './ReviewCard.js';
+import Loader from '../layout/Loader/Loader.js';
 
 const ProductDetails = ({match}) => {
     const dispatch = useDispatch();
@@ -21,53 +23,69 @@ const ProductDetails = ({match}) => {
     }
     return (
         <Fragment>
-            <div className="ProductDetails">
-                <div>
-                    <Carousel>
-                    {product.images &&
-                        product.images.map((item, i) => (
-                        <img
-                            className="CarouselImage"
-                            key={i}
-                            src={item.url}
-                            alt={`${i} Slide`}
-                        />
-                        ))}
-                    </Carousel>
-                </div>
-                <div>
-                    <div className="detailsBlock-1">
-                        <h2>{product.name}</h2>
-                        <p>Product # {product._id}</p>
-                    </div>
-                    <div className="detailsBlock-2">
-                        <ReactStars {...options} />
-                        <span>({product.numOfReviews} Reviews)</span>
-                    </div>
-                    <div className="detailsBlock-3">
-                        <h1>{`₹${product.price}`}</h1>
-                        <div className="detailsBlock-3-1">
-                            <div className="detailsBlock-3-1-1">
-                                <button>-</button>
-                                <input type="number" value='1'/>
-                                <button>+</button>
-                            </div>
-                            <button>Add to Cart</button>
+           {loading ? (
+                <Loader />
+            ) : (
+                <Fragment>
+                    <div className="ProductDetails">
+                        <div>
+                            <Carousel>
+                            {product.images &&
+                                product.images.map((item, i) => (
+                                <img
+                                    className="CarouselImage"
+                                    key={i}
+                                    src={item.url}
+                                    alt={`${i} Slide`}
+                                />
+                                ))}
+                            </Carousel>
                         </div>
-                        <p>
-                        Status: {" "}
-                        <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
-                            {product.Stock < 1 ? "OutOfStock" : "InStock"}
-                        </b>
-                        </p>
+                        <div>
+                            <div className="detailsBlock-1">
+                                <h2>{product.name}</h2>
+                                <p>Product # {product._id}</p>
+                            </div>
+                            <div className="detailsBlock-2">
+                                <ReactStars {...options} />
+                                <span>({product.numOfReviews} Reviews)</span>
+                            </div>
+                            <div className="detailsBlock-3">
+                                <h1>{`₹${product.price}`}</h1>
+                                <div className="detailsBlock-3-1">
+                                    <div className="detailsBlock-3-1-1">
+                                        <button>-</button>
+                                        <input type="number" value='1'/>
+                                        <button>+</button>
+                                    </div>
+                                    <button>Add to Cart</button>
+                                </div>
+                                <p>
+                                Status: {" "}
+                                <b className={product.Stock < 1 ? "redColor" : "greenColor"}>
+                                    {product.Stock < 1 ? "OutOfStock" : "InStock"}
+                                </b>
+                                </p>
+                            </div>
+                            <div className="detailsBlock-4">
+                                Description : <p>{product.description}</p>
+                            </div>
+                            <button className='submitReview'>Submit Review</button>
+                        </div>
                     </div>
-                    <div className="detailsBlock-4">
-                        Description : <p>{product.description}</p>
-                    </div>
-                    <button className='submitReview'>Submit Review</button>
-                </div>
-            </div>
-        </Fragment>
+                    <h3 className="reviewsHeading">REVIEWS</h3>
+                    { product.reviews && product.reviews[0] ? (
+                        <div className='reviews'>
+                            {product.reviews &&
+                                product.reviews.map((review) => <ReviewCard review={review} />)}
+                        </div>
+                    ):(
+                        <p className='noReviews'>NO REVIEWS YET</p>
+                    )
+                    }
+                </Fragment>
+                 )}
+            </Fragment>
     );
 };
 
