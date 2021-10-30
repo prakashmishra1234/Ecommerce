5947fc927b443d51bce96ab2baa5eb3559d34948
Prakash Mishra <pm9507041@gmail.com>
2021-10-30 20:27:34 +0530

    Created user registration form

--- frontend/src/component/User/LoginSignUp.js
+++ frontend/src/component/User/LoginSignUp.js
@@ -4,6 +4,7 @@ import Loader from '../layout/Loader/Loader';
 import { Link } from 'react-router-dom';
 import MailOutlineIcon from "@material-ui/icons/MailOutline";
 import LockOpenIcon from "@material-ui/icons/LockOpen";
+import FaceIcon from "@material-ui/icons/Face";
 
 function LoginSignUp() {
 
@@ -13,10 +14,45 @@ function LoginSignUp() {
 
     const [loginEmail, setLoginEmail] = useState("");
     const [loginPassword, setLoginPassword] = useState("");
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: "",
+    });
+
+    const {name, email, password} = user
+    const [avatar, setAvatar] = useState("");
+    const [avatarPreview, setAvatarPreview] = useState("/Profile.png");
 
     const loginSubmit = () => {
-        console.log('form submitted')
-    }
+        console.log('login form submitted')
+    };
+
+    const registerSubmit = (e) => {
+        e.preventDefault();
+        const myForm = new FormData();
+        myForm.set("name", name);
+        myForm.set("email", email);
+        myForm.set("password", password);
+        myForm.set("avatar", avatar);
+        //dispatch(register(myForm));
+        console.log('signUp form submitted');
+      };
+
+      const registerDataChange = (e) => {
+        if (e.target.name === "avatar") {
+          const reader = new FileReader();
+          reader.onload = () => {
+            if (reader.readyState === 2) {
+              setAvatarPreview(reader.result);
+              setAvatar(reader.result);
+            }
+          };
+          reader.readAsDataURL(e.target.files[0]);
+        } else {
+          setUser({ ...user, [e.target.name]: e.target.value });
+        }
+      };
 
     const switchTabs = (e, tab) => {
         if (tab === "login") {
@@ -67,6 +103,57 @@ function LoginSignUp() {
                         </div>
                         <Link to='/password/forgot'>Forget Password ?</Link>
                         <input type='submit' value='Login' className='loginBtn' />
+                    </form>
+                    <form
+                        className="signUpForm"
+                        ref={registerTab}
+                        encType="multipart/form-data"
+                        onSubmit={registerSubmit}
+                    >
+                        <div className="signUpName">
+                            <FaceIcon />
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                required
+                                name="name"
+                                value={name}
+                                onChange={registerDataChange}
+                            />
+                        </div>
+                        <div className="signUpEmail">
+                            <MailOutlineIcon />
+                            <input
+                                type="email"
+                                placeholder="Email"
+                                required
+                                name="email"
+                                value={email}
+                                onChange={registerDataChange}
+                            />
+                        </div>
+                        <div className="signUpPassword">
+                            <LockOpenIcon />
+                            <input
+                                type="password"
+                                placeholder="Password"
+                                required
+                                name="password"
+                                value={password}
+                                onChange={registerDataChange}
+                            />
+                        </div>
+
+                        <div id="registerImage">
+                            <img src={avatarPreview} alt="Avatar Preview" />
+                            <input
+                                type="file"
+                                name="avatar"
+                                accept="image/*"
+                                onChange={registerDataChange}
+                            />
+                        </div>
+                        <input type="submit" value="Register" className="signUpBtn" />
                     </form>
                 </div>
             </div>
