855773e1de7783cc2906e3e784cd8c7559d87480
Prakash Mishra <pm9507041@gmail.com>
2021-11-01 10:05:54 +0530

    Added cloudinary for image upload in backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const cloudinary = require("cloudinary");
 const connectDataBase = require('./config/database');
 
 //Handling Uncaught Exceptions
@@ -15,6 +16,12 @@ dotenv.config({path:"backend/config/config.env"});
 
 //connecting to database
 connectDataBase()
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`server working on http://localhost:${process.env.PORT}`)
