4f31f651a1ff4f890659bfaca77575a048b4b824
Prakash Mishra <pm9507041@gmail.com>
2021-11-02 10:31:12 +0530

    Created UpdateProfile component with css and implemented redux to fetch data and update

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -1,7 +1,8 @@
 import { LOGIN_SUCCESS, LOGIN_FAIL, LOGIN_REQUEST, CLEAR_ERRORS,
           REGISTER_USER_SUCCESS, REGISTER_USER_REQUEST, REGISTER_USER_FAIL,
           LOAD_USER_FAIL, LOAD_USER_SUCCESS, LOAD_USER_REQUEST,
-          LOGOUT_SUCCESS, LOGOUT_FAIL} from "../constants/userConstants"
+          LOGOUT_SUCCESS, LOGOUT_FAIL, UPDATE_PROFILE_REQUEST, 
+          UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_FAIL, UPDATE_PROFILE_RESET} from "../constants/userConstants"
 import axios from 'axios';
 
 // Login
@@ -59,6 +60,24 @@ export const logout = () => async (dispatch) => {
     dispatch({ type: LOGOUT_SUCCESS });
   } catch (error) {
     dispatch({ type: LOGOUT_FAIL, payload: error.response.data.message });
+  }
+};
+
+// Update Profile
+export const updateProfile = (userData) => async (dispatch) => {
+  try {
+    dispatch({ type: UPDATE_PROFILE_REQUEST });
+
+    const config = { headers: { "Content-Type": "multipart/form-data" } };
+
+    const { data } = await axios.put(`/api/v1/me/update`, userData, config);
+
+    dispatch({ type: UPDATE_PROFILE_SUCCESS, payload: data.success });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PROFILE_FAIL,
+      payload: error.response.data.message,
+    });
   }
 };
 
--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -1,7 +1,8 @@
 import { LOGIN_SUCCESS, LOGIN_FAIL, LOGIN_REQUEST, CLEAR_ERRORS,
           REGISTER_USER_FAIL, REGISTER_USER_SUCCESS, REGISTER_USER_REQUEST,
           LOAD_USER_SUCCESS, LOAD_USER_FAIL, LOAD_USER_REQUEST,
-          LOGOUT_SUCCESS, LOGOUT_FAIL} from "../constants/userConstants";
+          LOGOUT_SUCCESS, LOGOUT_FAIL, UPDATE_PROFILE_REQUEST, 
+          UPDATE_PROFILE_SUCCESS, UPDATE_PROFILE_FAIL, UPDATE_PROFILE_RESET} from "../constants/userConstants";
 
 export const userReducer = (state = {user: {} }, action )=> {
     switch (action.type) {
@@ -58,4 +59,39 @@ export const userReducer = (state = {user: {} }, action )=> {
         default:
             return state;
     }
-} 
+};
+
+export const profileReducer = (state = {}, action) => {
+  switch (action.type) {
+    case UPDATE_PROFILE_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+    case UPDATE_PROFILE_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        isUpdated: action.payload,
+      };
+    case UPDATE_PROFILE_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+
+    case UPDATE_PROFILE_RESET:
+      return {
+        ...state,
+        isUpdated: false,
+      };
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
